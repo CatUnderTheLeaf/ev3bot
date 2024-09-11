1c39391fd1befeeefe3a1e57a491eb26fbbffeaf
CatUnderTheLeaf <catundertheleaf@gmail.com>
2024-09-11 14:25:35 +0200

    CatUnderTheLeaf/roboVision#11

--- legobot.py
+++ legobot.py
@@ -1,14 +1,98 @@
 #!/usr/bin/env python3
 
-from ev3dev2.motor import OUTPUT_A, OUTPUT_B, OUTPUT_C, OUTPUT_D, MoveDifferential, SpeedRPS, SpeedRPM, SpeedPercent, LargeMotor
+from ev3dev2.motor import MoveDifferential, OUTPUT_A, OUTPUT_D, SpeedRPS, SpeedRPM, SpeedPercent, LargeMotor, SpeedNativeUnits
 from ev3dev2.wheel import EV3Tire
 from ev3dev2.sound import Sound
 from ev3dev2.led import Leds
 
-class LegoBot(MoveDifferential):
+class MoveSteerDiff(MoveDifferential):
+    """
+    A combination of MoveDifferential and MoveSteering clasess
+    to control the robot with `cmd_vel` message type
+
+    Args:
+        MoveDifferential (_type_): _description_
+    """
+    def __init__(self, left_motor, right_motor, wheel_distance_mm):
+        MoveDifferential.__init__(self, left_motor, right_motor, EV3Tire, wheel_distance_mm)
+
+    def steer_on(self, steering, speed):
+        """
+        Start rotating the motors according to the provided ``steering`` and
+        ``speed`` forever.
+        """
+        (left_speed, right_speed) = self.get_speed_steering(steering, speed)
+        MoveDifferential.on(self, SpeedNativeUnits(left_speed), SpeedNativeUnits(right_speed))
+
+    def get_speed_steering(self, steering, speed):
+        """
+        Calculate the speed_sp for each motor in a pair to achieve the specified
+        steering. Note that calling this function alone will not make the
+        motors move, it only calculates the speed. A run_* function must be called
+        afterwards to make the motors move.
+
+        steering [-100, 100]:
+            * -100 means turn left on the spot (right motor at 100% forward, left motor at 100% backward),
+            *  0   means drive in a straight line, and
+            *  100 means turn right on the spot (left motor at 100% forward, right motor at 100% backward).
+
+        speed:
+            The speed that should be applied to the outmost motor (the one
+            rotating faster). The speed of the other motor will be computed
+            automatically.
+        """
+
+        assert steering >= -100 and steering <= 100,\
+            "{} is an invalid steering, must be between -100 and 100 (inclusive)".format(steering)
+
+        # We don't have a good way to make this generic for the pair... so we
+        # assume that the left motor's speed stats are the same as the right
+        # motor's.
+        speed = self.left_motor._speed_native_units(speed)
+        left_speed = speed
+        right_speed = speed
+        speed_factor = (50 - abs(float(steering))) / 50
+
+        if steering >= 0:
+            right_speed *= speed_factor
+        else:
+            left_speed *= speed_factor
+
+        return (left_speed, right_speed)
     
-    def __init__(self, wheel_distance_mm):
-        MoveDifferential.__init__(self, OUTPUT_A, OUTPUT_D, EV3Tire, wheel_distance_mm)
+    def steer_on_for_rotations(self, steering, speed, rotations, brake=True, block=True):
+        """
+        Rotate the motors according to the provided ``steering``.
+
+        The distance each motor will travel follows the rules of :meth:`MoveTank.on_for_rotations`.
+        """
+        (left_speed, right_speed) = self.get_speed_steering(steering, speed)
+        MoveDifferential.on_for_rotations(self, SpeedNativeUnits(left_speed), SpeedNativeUnits(right_speed), rotations, brake,
+                                  block)
+
+    def steer_on_for_degrees(self, steering, speed, degrees, brake=True, block=True):
+        """
+        Rotate the motors according to the provided ``steering``.
+
+        The distance each motor will travel follows the rules of :meth:`MoveTank.on_for_degrees`.
+        """
+        (left_speed, right_speed) = self.get_speed_steering(steering, speed)
+        MoveDifferential.on_for_degrees(self, SpeedNativeUnits(left_speed), SpeedNativeUnits(right_speed), degrees, brake,
+                                block)
+
+    def steer_on_for_seconds(self, steering, speed, seconds, brake=True, block=True):
+        """
+        Rotate the motors according to the provided ``steering`` for ``seconds``.
+        """
+        (left_speed, right_speed) = self.get_speed_steering(steering, speed)
+        MoveDifferential.on_for_seconds(self, SpeedNativeUnits(left_speed), SpeedNativeUnits(right_speed), seconds, brake,
+                                block)
+
+
+class LegoBot(MoveSteerDiff):
+    
+    def __init__(self, left_motor, right_motor, wheel_distance_mm):
+        MoveSteerDiff.__init__(self, left_motor, right_motor, wheel_distance_mm)
         """ 
         LegoBot Class inherits all usefull stuff for differential drive
         and adds sound, LEDs
@@ -27,7 +111,11 @@ class LegoBot(MoveDifferential):
 
         self.sound.speak('Hello, my name is EV3!')
 
-    def turn_off(self):        
+    def turn_off(self):
+        """
+        A sequence to shutdown the robot
+
+        """      
         # stop odometry thread
         # self.odometry_stop()
 
@@ -36,6 +124,13 @@ class LegoBot(MoveDifferential):
         self.leds.set_color("LEFT", "BLACK")
         self.leds.set_color("RIGHT", "BLACK")
     
-    def move(self,steering, speed, seconds):
-        self.on_for_seconds(steering, SpeedPercent(speed), seconds)
-        
+    def move(self,steering, speed):
+        """
+        Rotates both motors with the specified
+        steering and speed
+
+        Args:
+            steering (int): angular velocity
+            speed (int): linear velocity
+        """
+        self.steer_on(steering, SpeedPercent(speed))      
--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python3
 
 import os
+from os import strerror
 
 import sys
+
 import logging
+import yaml
 
 from legobot import LegoBot
+
+import paho.mqtt.client as mqtt
 
 
 # set logger to display on both EV3 Brick and console
@@ -19,12 +24,67 @@ if __name__ == '__main__':
     # set large letters on ev3 display
     os.system('setfont Lat15-TerminusBold14')
     
-    STUD_MM = 8
-    DIST_BTW_WHEELS = 15 * STUD_MM
+    # load configuration
+    try:
+        with open("config.yaml", mode="r") as f:
+            config = yaml.safe_load(f)
+    except OSError as error:
+        print(strerror(error.errno))
+    except yaml.YAMLError as exc:
+        print(exc)
+
+    if config is None:
+        print('empty config file')
+        sys.exit()
+
+    DIST_BTW_WHEELS = config['stud_num'] * config['stud_mm']
    
-    bot = LegoBot(DIST_BTW_WHEELS)
+    # create a robot instance
+    bot = LegoBot(config['left_motor'], config['right_motor'], DIST_BTW_WHEELS)
 
-    # move 30# right, 75% speed, 10 seconds
-    bot.move(30, 75, 10)
 
-    bot.turn_off()
+    # MQTT subscriber functions
+    def on_connect(client, userdata, flags, rc):
+        """
+        connect to the MQTT client and subscribe to a topic
+
+        Args:
+            client (Client) - the client instance for this callback
+            userdata - the private user data
+            connect_flags (ConnectFlags) - the flags for this connection
+            reason_code (ReasonCode) - the connection reason code received from the broken
+        """
+        print("Connected with result code "+str(rc))
+        client.subscribe(config['cmd_vel'])
+
+    def on_message(client, userdata, msg):
+        """
+        recieve `cmd_vel` message and 
+        move the robot accordingly
+
+        Args:
+            client (Client) - the client instance for this callback
+            userdata - the private user data
+            message (MQTTMessage) - the received message
+        """
+        if msg.payload.decode() == 'q':
+            client.disconnect()
+            print('turning off')
+            bot.turn_off()
+        else:
+            speed, steer = msg.payload.decode().split(' ')
+            if steer == '0' and speed == '0':
+                print('stopping')
+                bot.stop()
+            else:
+                print(msg.payload.decode())
+                bot.move(int(steer), int(speed))
+
+    # create and run MQTT client to receive messages
+    client = mqtt.Client()
+    client.connect(config['broker_ip'], config['port'], config['keep_alive'])
+
+    client.on_connect = on_connect
+    client.on_message = on_message
+
+    client.loop_forever()
